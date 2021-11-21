f578494122e28862b966c51c6e515ca99b8b9593
Yoga Moorthy R <yogamoorthy_r@hotmail.com>
2021-11-21 06:17:47 +0530

    Initialized repository added 4 files Test.py, Test1, get-pip & Bitcoin.csv

--- Test.py
+++ Test.py
@@ -34,3 +34,11 @@ areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 # Change for loop to use enumerate() and update print()
 for index, area in enumerate(areas) :
     print("room " + str(index) + ": " + str(area))
+
+
+    def foo(a, b):
+        """State what function does here"""
+        # Computation performed here
+        return x, y
+
+
