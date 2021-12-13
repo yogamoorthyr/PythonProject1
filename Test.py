2bd8911332382b6dca1ce81dd09f248d60a714ed
Yoga Moorthy R <yogamoorthy_r@hotmail.com>
2021-12-13 16:58:12 +0530

    Initialized repository added 4 files Test.py, Test1, get-pip & Bitcoin.csv

--- Test.py
+++ Test.py
@@ -1,85 +1,20 @@
 
 
-x=[1,4,10, 12,2,8,10,12]
-print(x)
+print ('*' * 10)
 
-x=(1,4,10,9,5,7)
-print (x)
-import numpy as np
+full_name = 'John Smith'
+age = 20
+is_new = True
+print (is_new)
 
-print (x)
-
-new_x = np.array(x)
-x+x
-print (x+x)
-
-import matplotlib.pyplot as plt
+name = input('What is your name? ')
+print ('Hi ' + name)
 
 
-import pandas as pd
-names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
-dr =  [True, False, False, False, True, True, True]
-cpc = [809, 731, 588, 18, 200, 70, 45]
-cars_dict = { 'country':names, 'drives_right':dr, 'cars_per_cap':cpc }
-cars = pd.DataFrame(cars_dict)
-print(cars)
-row_labels = ['US', 'AUS', 'JPN', 'IN', 'RU', 'MOR', 'EG']
-cars.index=row_labels
-print(cars)
-print (cars.index)
+name = input('What is your name? ')
+favourite_colour = input('What Colour do you like? ')
+print (name + ' likes ' + favourite_colour + ' colour')
 
-# areas list
-areas = [11.25, 18.0, 20.0, 10.75, 9.50]
-
-# Change for loop to use enumerate() and update print()
-for index, area in enumerate(areas) :
-    print("room " + str(index) + ": " + str(area))
-
-
-    def foo(a, b):
-        """State what function does here"""
-        # Computation performed here
-        return x, y
-
-import numpy as np
-
-
-# Assign filename: file
-file = open(r'C:\Users\Dell\PycharmProjects\pythonProject\PythonProject1\seaslugs.dat.txt')
-
-# Import file: data
-data = np.loadtxt(file, delimiter='\t', dtype=str)
-
-# Print the first element of data
-print(data[0])
-print (data)
-
-import pandas as pd
-
-import pandas as pd
-
-
-import platform
-print('Python: ', platform.python_version())
-print('pandas: ', pd.__version__)
-
-
-
-import SAS7BDAT
-
-file = open(r'C:\Users\Dell\PycharmProjects\pythonProject\PythonProject1\beer.sas7bdat')
-
-from sas7bdat import SAS7BDAT
-with SAS7BDAT (r'C:\Users\Dell\PycharmProjects\pythonProject\PythonProject1\beer.sas7bdat') as file: df_sas = file.to_data_frame()
-print(df_sas.head())
-pd.DataFrame.hist(df_sas[['P']])
-plt.ylabel('count')
-plt.show()
-
-import pandas as pd
-adeff2 = pd.read_sas(r'C:\Users\Dell\PycharmProjects\pythonProject\PythonProject1\beer.sas7bdat')
-print(adeff2)
-
-
-import scipy.io
-
+birth_year = input('Birth year: ')
+age = 2021 - int(birth_year)
+print (age)
