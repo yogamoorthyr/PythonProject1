65770d332aa91128c0009d37921378f80eb945e6
Yoga Moorthy R <yogamoorthy_r@hotmail.com>
2021-12-19 07:44:07 +0530

    Initialized repository added 4 files Test.py, Test1, get-pip & Bitcoin.csv

--- Test.py
+++ Test.py
@@ -1,20 +1,9 @@
+first = 'John'
+last = 'Smith'
 
+print (first)
+message = first + ' ['+ last + '] is a coder'
+print (message)
+msg = f'{first} [{last}] is a coder'
+print (msg)
 
-print ('*' * 10)
-
-full_name = 'John Smith'
-age = 20
-is_new = True
-print (is_new)
-
-name = input('What is your name? ')
-print ('Hi ' + name)
-
-
-name = input('What is your name? ')
-favourite_colour = input('What Colour do you like? ')
-print (name + ' likes ' + favourite_colour + ' colour')
-
-birth_year = input('Birth year: ')
-age = 2021 - int(birth_year)
-print (age)
